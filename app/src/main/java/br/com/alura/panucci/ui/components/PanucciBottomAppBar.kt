4bea73bd6161c42bc6c388e667590d5ec0792065
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-24 16:43:08 -0300

    Ajustados códigos que apresentam ou não os componentes visuasis para que utilizem do novo padrão.

--- app/src/main/java/br/com/alura/panucci/ui/components/PanucciBottomAppBar.kt
+++ app/src/main/java/br/com/alura/panucci/ui/components/PanucciBottomAppBar.kt
@@ -1,5 +1,9 @@
 package br.com.alura.panucci.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.RestaurantMenu
+import androidx.compose.material.icons.outlined.LocalBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -8,15 +12,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.alura.panucci.navigation.AppDestination
 import br.com.alura.panucci.navigation.bottomAppBarItems
+import br.com.alura.panucci.navigation.drinksRoute
+import br.com.alura.panucci.navigation.highlightsListRoute
+import br.com.alura.panucci.navigation.menuRoute
 import br.com.alura.panucci.ui.theme.PanucciTheme
 
-class BottomAppBarItem(
+open class BottomAppBarItem(
     val label: String,
     val icon: ImageVector,
-    val destination: AppDestination,
-)
+    val destination: String,
+) {
+    object HighlightsList : BottomAppBarItem(
+        label = "Destaques",
+        icon = Icons.Filled.AutoAwesome,
+        destination = highlightsListRoute
+    )
+
+    object Menu : BottomAppBarItem(
+        label = "Menu",
+        icon = Icons.Filled.RestaurantMenu,
+        destination = menuRoute
+    )
+
+    object Drinks : BottomAppBarItem(
+        label = "Bebidas",
+        icon = Icons.Outlined.LocalBar,
+        destination = drinksRoute
+    )
+}
 
 @Composable
 fun PanucciBottomAppBar(
@@ -29,14 +53,12 @@ fun PanucciBottomAppBar(
         items.forEach {
             val label = it.label
             val icon = it.icon
-            NavigationBarItem(
-                icon = { Icon(icon, contentDescription = label) },
+            NavigationBarItem(icon = { Icon(icon, contentDescription = label) },
                 label = { Text(label) },
                 selected = item.label == label,
                 onClick = {
                     onItemChange(it)
-                }
-            )
+                })
         }
     }
 }
@@ -46,8 +68,7 @@ fun PanucciBottomAppBar(
 fun PanucciBottomAppBarPreview() {
     PanucciTheme {
         PanucciBottomAppBar(
-            item = bottomAppBarItems.first(),
-            items = bottomAppBarItems
+            item = bottomAppBarItems.first(), items = bottomAppBarItems
         )
     }
 }
